c59b1a92be0ad5cfbbeda6c0087e19b6979b0758
ttandon <tanay@clipped.me>
2014-06-09 15:24:22 -0700

    new shit

--- upload_file.php
+++ upload_file.php
@@ -1,10 +1,36 @@
 <?php
-if ($_FILES["file"]["error"] > 0) {
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
-} else {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+$allowedExts = array(
+  "pdf", 
+  "doc", 
+  "docx"
+); 
+
+$allowedMimeTypes = array( 
+  'application/msword',
+  'text/pdf',
+  'image/gif',
+  'image/jpeg',
+  'image/png'
+);
+
+$extension = end(explode(".", $_FILES["file"]["name"]));
+
+if ( 20000 < $_FILES["file"]["size"]  ) {
+  die( 'Please provide a smaller file [E/1].' );
 }
+
+if ( ! ( in_array($extension, $allowedExts ) ) ) {
+  die('Please provide another file type [E/2].');
+}
+
+if ( in_array( $_FILES["file"]["type"], $allowedMimeTypes ) ) 
+{      
+ move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]); 
+}
+else
+{
+die('Please provide another file type [E/3].');
+}
+
 ?>
